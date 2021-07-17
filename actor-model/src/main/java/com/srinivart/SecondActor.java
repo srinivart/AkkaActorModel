4ebead24b615999cf32903deb05f01f03d13b70b
“srinivart” <srinivart@gmail.com>
2021-07-17 14:03:37 +0530

    Akka Actor Model

---
+++ actor-model/src/main/java/com/srinivart/SecondActor.java
@@ -0,0 +1,22 @@
+package com.srinivart;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+
+import java.time.Duration;
+
+public class SecondActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(String.class, s -> {
+                    System.out.println("Received String message: {} "+s);
+                    System.out.println(sender().path());
+                    MainActor.system.scheduler().schedule(Duration.ZERO,Duration.ofSeconds(3), () -> {
+                        System.out.println("Working on task");}, MainActor.system.dispatcher());
+                })
+                .matchAny(o-> System.out.println("Unknown message received"))
+                .build();
+    }
+
+}
